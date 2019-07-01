a3a1954850c4b0898dabddbf1b9b3e4fb2141a9e
ObiWonBinobi77 <51964850+ObiWonBinobi77@users.noreply.github.com>
2019-07-01 09:41:06 -0700

    Add files via upload

---
+++ hello2.cpp
@@ -0,0 +1,16 @@
+//hello2.cpp bob
+// $g++ hello2.cpp -o hello2.o
+#include <iostream>
+using namespace std;
+
+int main(){
+ int i;
+ for (;;){
+  cout<<i<<" "<<j<<" ";
+  i = i + 1;
+  j++;
+ }
+ return 0;
+ 
+}
+
